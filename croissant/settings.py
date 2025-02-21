1deccf84095599758d88e9d2da1aae0debdf8db0
nosh11 <101454424+nosh11@users.noreply.github.com>
2025-02-21 17:15:09 +0900

    fix something

--- croissant/settings.py
+++ croissant/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-q710#gs0q#n!$y7apgjgf1e%%c@z%lzsg&vuv#3sd&2d+gjx$v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', 'False') == 'True'
+DEBUG = 'False'
 
-ALLOWED_HOSTS = ['.azurewebsites.net']
+ALLOWED_HOSTS = ['.azurewebsites.net', 'localhost']
 
 
 # Application definition
