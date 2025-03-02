8c379b1ec812629cc36eb8374cc1fabe785deb82
nosh11 <101454424+nosh11@users.noreply.github.com>
2025-03-02 23:16:30 +0900

    v

--- croissant/settings.py
+++ croissant/settings.py
@@ -98,7 +98,7 @@ if os.getenv("DJANGO_ENV") == "production":
             'PORT': os.getenv('PGPORT'),
             'OPTIONS': {
                 'charset': 'utf8mb4',
-                'ssl': {'ca': "/var/www/html/DigiCertGlobalRootCA.crt.pem"},
+                'ssl': {'ca': "/home/site/wwwroot/ssl.crt.pem"},
             }
         }
     }
