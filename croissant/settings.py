705a9734093f6355f8cfdc0e465c2ea58c6f9ac9
nosh11 <101454424+nosh11@users.noreply.github.com>
2025-03-02 17:36:35 +0900

    a

--- croissant/settings.py
+++ croissant/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-q710#gs0q#n!$y7apgjgf1e%%c@z%lzsg&vuv#3sd&2d+gjx$v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'False'
 
-ALLOWED_HOSTS = ['croissantmc-cmb5drgfcpffg6bm.japaneast-01.azurewebsites.net', 'localhost', "169.254.129.5", "169.254.131.5"]
+ALLOWED_HOSTS = ['croissantmc-cmb5drgfcpffg6bm.japaneast-01.azurewebsites.net', 'localhost', "169.254.131.*", "169.254.129.*"]
 
 CSRF_TRUSTED_ORIGINS = ['https://localhost:8000', 'https://croissantmc-cmb5drgfcpffg6bm.japaneast-01.azurewebsites.net']
 
